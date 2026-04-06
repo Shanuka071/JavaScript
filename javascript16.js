14c10b66627cd083db74bcb8b6dec69821f4e7d6
Shanuka071 <shanukaaspu@gmail.com>
2026-04-06 23:23:56 +0530

    feat:repush the code that are not pushed successfully

---
+++ javascript16.js
@@ -0,0 +1,22 @@
+//Make a simple greeting function program
+function my_name(name){
+    return `Hello ${name}`;
+}
+function my_age(age){
+    return `My age is ${age}`;
+}
+function my_city(city){
+    return `I live in ${city}`;
+}
+function my_homeTown(homeTown){
+    return `My home town is ${homeTown}`;
+}
+function my_country(country){
+    return `My country is ${country}`;
+}
+function my_country(country){
+    return `My country is ${country}`;
+}
+function my_hobby(hobby){
+    return `My hobby is ${hobby}`;
+}
